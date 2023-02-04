e19d80f5e05d2beff48cac58db9a4211f3913cbb
shakibHossain <shossain@opusonesolutions.com>
2023-02-04 08:04:09 -0500

    Add dummy products to product list

--- src/lib/features/components/product-list/product-list.component.ts
+++ src/lib/features/components/product-list/product-list.component.ts
@@ -6,8 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.sass'],
 })
 export class ProductListComponent {
-  productName: string = 'Product 1';
-  productDescription: string = 'Product 1 description';
+  name1: string = 'Apple iPhone 12';
+  description1: string =
+    'This product will have a battery that exceeds 80% capacity relative to new';
+  image1: string =
+    'https://m.media-amazon.com/images/I/513NI5xpYjL.__AC_SX300_SY300_QL70_ML2_.jpg';
+  price1: number = 598;
+
+  name2: string = 'Apple iPhone 11';
+  description2: string =
+    'This product comes with a 90-day supplier-backed warranty';
+  image2: string =
+    'https://m.media-amazon.com/images/I/51cPOOgzp0L._AC_SX425_.jpg';
+  price2: number = 512;
 
   isDisabled: boolean = false;
+
+  handleClick() {
+    alert('Here');
+  }
 }
