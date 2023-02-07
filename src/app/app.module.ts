b7a7ccdd61b0a8de720f1a1a2d544fc5b81ad2f7
shakibHossain <shossain@opusonesolutions.com>
2023-02-07 14:11:10 -0500

    wip. add button and navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ProductsModule } from 'src/lib/features/components/products/products.module';
 import { CartComponent } from 'src/lib/features/components/cart/components/cart.component';
+import { ButtonComponent } from '../lib/shared/button/button.component';
+import { NavbarComponent } from '../lib/core/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, CartComponent],
+  declarations: [AppComponent, CartComponent, ButtonComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ProductsModule],
   bootstrap: [AppComponent],
 })
